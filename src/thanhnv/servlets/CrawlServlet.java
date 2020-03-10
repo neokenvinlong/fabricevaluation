baa0fe0c959502fd85b9c9dd01445dd599a51ab5
effortmanagement <winsupersentai@gmail.com>
2020-03-10 21:32:26 +0700

    test1

--- src/thanhnv/servlets/CrawlServlet.java
+++ src/thanhnv/servlets/CrawlServlet.java
@@ -14,20 +14,20 @@ public class CrawlServlet extends HttpServlet {
     public void init() throws ServletException {
         String realPath = this.getServletContext().getRealPath("/");
         PageCrawler categoryCrawler = new CategoryCrawler("https://www.uniqlo.com/us/en/men/outerwear-and-blazers", realPath);
-        TaskCrawler taskCrawlerCategory = new TaskCrawler("OUTBLA", categoryCrawler);
-        taskCrawlerCategory.run();
-//        PageCrawler outBlaCrawler = new OutBlaCrawler("https://www.uniqlo.com/us/en/men/outerwear-and-blazers", realPath);
-////        TaskCrawler taskCrawlerOutBla = new TaskCrawler("OUTBLA", outBlaCrawler);
-////        PageCrawler swSwCrawler = new SwSwCrawler("https://www.uniqlo.com/us/en/men/sweatshirts-and-sweatpants", realPath);
-////        TaskCrawler taskCrawlerSwSw = new TaskCrawler("SWSW", swSwCrawler);
-////        PageCrawler sweCrawler = new SweCrawler("https://www.uniqlo.com/us/en/men/sweaters", realPath);
-////        TaskCrawler taskCrawlerSwe = new TaskCrawler("SWE", sweCrawler);
-////        PageCrawler casualCrawler = new CasualCrawler("https://www.uniqlo.com/us/en/men/casual-shirts", realPath);
-////        TaskCrawler taskCrawlerCasual = new TaskCrawler("CASUAL", casualCrawler);
-////        taskCrawlerOutBla.run();
-////        taskCrawlerSwSw.run();
-////        taskCrawlerSwe.run();
-////        taskCrawlerCasual.run();
+//        TaskCrawler taskCrawlerCategory = new TaskCrawler("OUTBLA", categoryCrawler);
+//        taskCrawlerCategory.run();
+        PageCrawler outBlaCrawler = new OutBlaCrawler("https://www.uniqlo.com/us/en/men/outerwear-and-blazers", realPath);
+        TaskCrawler taskCrawlerOutBla = new TaskCrawler("OUTBLA", outBlaCrawler);
+        PageCrawler swSwCrawler = new SwSwCrawler("https://www.uniqlo.com/us/en/men/sweatshirts-and-sweatpants", realPath);
+        TaskCrawler taskCrawlerSwSw = new TaskCrawler("SWSW", swSwCrawler);
+        PageCrawler sweCrawler = new SweCrawler("https://www.uniqlo.com/us/en/men/sweaters", realPath);
+        TaskCrawler taskCrawlerSwe = new TaskCrawler("SWE", sweCrawler);
+        PageCrawler casualCrawler = new CasualCrawler("https://www.uniqlo.com/us/en/men/casual-shirts", realPath);
+        TaskCrawler taskCrawlerCasual = new TaskCrawler("CASUAL", casualCrawler);
+        taskCrawlerOutBla.run();
+        taskCrawlerSwSw.run();
+        taskCrawlerSwe.run();
+        taskCrawlerCasual.run();
 
 //        Timer timer = new Timer();
 //        timer.scheduleAtFixedRate(taskCrawler, 0, 1000);
